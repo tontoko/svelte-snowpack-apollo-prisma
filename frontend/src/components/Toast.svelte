e507167889b138e9deba1e0b9c8902f60c76dc8e
Hiraki Tomohiko <hirakitomohiko@gmail.com>
2021-02-04 00:30:19 +0900

    toastのアイコンが変わるように修正

--- frontend/src/components/Toast.svelte
+++ frontend/src/components/Toast.svelte
@@ -3,24 +3,50 @@
     type?: 'primary' | 'success' | 'info' | 'warning' | 'danger'
     text: string
   }
-  let toastText = writable('')
+  type Icon = 'info-circle' | 'check2-circle' | 'gear' | 'exclamation-triangle' | 'exclamation-octagon'
+  let toastProps = writable({ text: '', icon: 'info-circle' })
   let toastRef: HTMLElement & { toast(): void; type: ToastProps['type'] }
   export const openToast = async ({ type = 'primary', text }: ToastProps) => {
     if (!toastRef) return
-    toastText.set(text)
+    let icon: Icon
+    switch (type) {
+      case 'primary':
+        icon = 'info-circle'
+        break
+      case 'success':
+        icon = 'check2-circle'
+        break
+      case 'info':
+        icon = 'gear'
+        break
+      case 'warning':
+        icon = 'exclamation-triangle'
+        break
+      case 'danger':
+        icon = 'exclamation-octagon'
+        break
+    }
+    toastProps.set({ text, icon })
     toastRef.type = type
+    await tick()
     toastRef.toast()
   }
 </script>
 
 <script>
+  import { tick } from 'svelte'
+
   import { writable } from 'svelte/store'
   let innerText = ''
-  toastText.subscribe((v) => (innerText = v))
+  let icon = 'info-circle'
+  toastProps.subscribe((v) => {
+    innerText = v.text
+    icon = v.icon
+  })
 </script>
 
 <sl-alert type="primary" closable class="alert-closable" duration="3000" bind:this="{toastRef}">
-  <sl-icon slot="icon" name="info-circle"></sl-icon>
+  <sl-icon slot="icon" name="{icon}"></sl-icon>
   {innerText}
 </sl-alert>
 
